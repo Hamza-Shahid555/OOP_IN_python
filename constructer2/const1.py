87040058cb873913036823fa084e3595df89136e
Hamza_shahid555 <zhsmarketer@gmail.com>
2026-08-04 22:45:25 +0500

    Add files via upload

---
+++ constructer2/const1.py
@@ -0,0 +1,7 @@
+class Student:
+    def __init__(self , fullname):
+        self.name= fullname
+        print(f"{self.name} is a student of oop")
+        #print(f"{self.name} is a student of oop")
+
+s1=Student("Hamza shahid")
